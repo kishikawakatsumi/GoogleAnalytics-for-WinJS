b0801838a7b5b9a1c26d6773bdd13633f800be7e
kishikawakatsumi <kishikawakatsumi@mac.com>
2012-11-08 12:58:41 +0900

    First commit.

---
+++ GoogleAnalytics/js/tracker.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+
+    var Tracker = WinJS.Class.define(
+        function (webPropertyId) {
+            this.webPropertyId = webPropertyId;
+            this.tracker = new WindowsRuntimeGoogleAnalytics.Tracker(webPropertyId);
+        },
+
+        {
+            setCustomVariable: function (index, name, value) {
+                var that = this;
+                that.tracker.setCustomVariable(index, name, value);
+            },
+
+            trackPageView: function (page) {
+                var that = this;
+                that.tracker.trackPageView(page);
+            },
+
+            trackCurrentPageView: function () {
+                var that = this;
+                var page = Application.navigator.pageControl.uri.replace("ms-appx://" + Windows.ApplicationModel.Package.current.id.name.toLowerCase(), "");
+                that.trackPageView(page);
+            },
+
+            trackEvent: function (category, action, label) {
+                var that = this;
+                that.tracker.trackEvent(category, action, label);
+            }
+        });
+
+    WinJS.Namespace.define("GoogleAnalytics", {
+        Tracker: new Tracker("UA-4291014-7")
+    });
+})();
---
+++ WindowsRuntimeGoogleAnalytics/Tracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nascent.GoogleAnalytics;
+
+namespace WindowsRuntimeGoogleAnalytics
+{
+    public sealed class Tracker
+    {
+        public string webPropertyId { get; set; }
+
+        public Tracker(string id)
+        {
+            webPropertyId = id;
+        }
+
+        public void SetCustomVariable(int index, string name, string value)
+        {
+            AnalyticsTracker tracker = AnalyticsTracker.GetInstance(webPropertyId);
+            tracker.SetCustomVariable(index, name, value);
+        }
+
+        public void TrackPageView(string page)
+        {
+            AnalyticsTracker tracker = AnalyticsTracker.GetInstance(webPropertyId);
+            tracker.TrackPageView(page);
+        }
+
+        public void TrackEvent(string category, string action)
+        {
+            AnalyticsTracker tracker = AnalyticsTracker.GetInstance(webPropertyId);
+            tracker.TrackEvent(category, action, null, null);
+        }
+
+        public void TrackEvent(string category, string action, string label)
+        {
+            AnalyticsTracker tracker = AnalyticsTracker.GetInstance(webPropertyId);
+            tracker.TrackEvent(category, action, label, null);
+        }
+    }
+}
